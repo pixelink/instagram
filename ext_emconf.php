782e8bd282afa9515482a0aa2b375d223820d20b
Benjamin Riezler <b.riezler@pixel-ink.de>
2020-06-03 15:10:54 +0200

    [FEATURE] Add TYPO3 v9.5 compatiblity

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '9.5.0-9.5.99',
             'php' => '7.2.0-7.99.99'
         ],
         'conflicts' => [],
